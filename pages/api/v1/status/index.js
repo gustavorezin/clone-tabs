70760f5a2a2ff20f2eafd624f4f3762ef1170707
Gustavo Rezin Durigon <77802067+gustavorezin@users.noreply.github.com>
2024-02-16 20:46:20 +0000

    Inicio aula SQL Injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 AS sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Deu certo: 200" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = "local_db";
+  const databaseOpennedConnectionsResult = await database.query(
+    `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
+  );
+  const databaseOpennedConnectionsValue =
+    databaseOpennedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpennedConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
